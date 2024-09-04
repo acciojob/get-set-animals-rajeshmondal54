c6217b6b1b97b4e4ca85e5b1a7e646206a71681d
rajeshmondal54 <rm4223536@gmail.com>
2024-09-05 04:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-// Define the Animal class
-class Animal {
     constructor(species) {
-        this._species = species; // Store species in a private variable
+        this._species = species;
     }
 
-    // Getter for species
     get species() {
         return this._species;
     }
 
-    // Method to make a sound
+
     makeSound() {
         console.log(`The ${this.species} makes a sound`);
     }
 }
 
-// Define the Dog class that extends Animal
 class Dog extends Animal {
-    // Method to log "woof" to the console
     bark() {
         console.log("woof");
     }
 }
 
-// Define the Cat class that extends Animal
 class Cat extends Animal {
-    // Method to log "purr" to the console
     purr() {
         console.log("purr");
     }
 }
 
-// Expose the classes to the window object for Cypress tests
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
