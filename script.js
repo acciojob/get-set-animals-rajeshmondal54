c29c798f45972ffaa0fe24c3a0f482f1ce6520aa
rajeshmondal54 <rm4223536@gmail.com>
2024-09-05 10:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,18 +12,23 @@
     }
 }
 
+// Define the Dog class that extends Animal
 class Dog extends Animal {
+    // Method to log "woof" to the console
     bark() {
         console.log("woof");
     }
 }
 
+// Define the Cat class that extends Animal
 class Cat extends Animal {
+    // Method to log "purr" to the console
     purr() {
         console.log("purr");
     }
 }
 
+// Expose the classes to the window object for Cypress tests
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
